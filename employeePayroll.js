c1ea0dac3f6963fba603d0ed178dec45026d4dd9
KumarB95 <kumarbalingal@gmail.com>
2021-11-29 15:08:40 +0530

    Refactored : Display employee details from Json

--- employeePayroll.js
+++ employeePayroll.js
@@ -2,24 +2,52 @@ window.addEvenListener('DOMContentLoaded',(event) =>{
         createInnerHtml();
 });
 
-//Templete features 
-const createInnerHtml = () =>{
-    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
-                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+// //Templete features 
+// const createInnerHtml = () =>{
+//     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+//                         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
 
-const innerHtml = `'${headerHtml}
-    <tr>
-        <td><img class="profile" src="Asset/Profile pic/Ellipse -2.png" alt=""></td> 
-        <td>Narayan Mahadevan</td>
-        <td>Male</td>
-        <td><div class="dept-label">HR<</div><div class='dept-label'>Finance</div></td> 
-        <td>3000000</td>
-        <td>1 Nov 2020</td>
+// const innerHtml = `'${headerHtml}
+//     <tr>
+//         <td><img class="profile" src="Asset/Profile pic/Ellipse -2.png" alt=""></td> 
+//         <td>Narayan Mahadevan</td>
+//         <td>Male</td>
+//         <td><div class="dept-label">HR<</div><div class='dept-label'>Finance</div></td> 
+//         <td>3000000</td>
+//         <td>1 Nov 2020</td>
+//         <td>
+//         <img name="1" onclick="remove(this)" src="Asset/icons/delete-black-18dp.svg" alt="delete">
+//         <img name="1" onclick="update(this)" src="Asset/icons/create-black-18dp.svg" alt="edit">
+// </td>
+// </tr>
+// `;
+//     document.querySelector('#table-display').innerHtml = innerHtml;
+// }
+
+const createInnerHtml = () =>{
+
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+    "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollList of empPayrollList){
+        innerHtml = `${headerHtml}
+        <tr>
+            <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td> 
+        <td>"${empPayrollData._name}"</td>
+        <td>"${empPayrollData._gender}"</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td> 
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
         <td>
-        <img name="1" onclick="remove(this)" src="Asset/icons/delete-black-18dp.svg" alt="delete">
-        <img name="1" onclick="update(this)" src="Asset/icons/create-black-18dp.svg" alt="edit">
-</td>
-</tr>
-`;
+            <img name="${empPayrollData._id}" onclick="remove(this)" 
+            src="Asset/icons/delete-black-18dp.svg" alt="delete">
+            <img name="${empPayrollData._id}" onclick="update(this)" 
+            src="Asset/icons/create-black-18dp.svg" alt="edit">
+        </td>
+    </tr>`; 
+    }
     document.querySelector('#table-display').innerHtml = innerHtml;
 }
+    
